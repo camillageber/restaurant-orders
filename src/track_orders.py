ba188157279aec98ee187576e138e547f752d210
camillageber <camilla.geber@gmail.com>
2023-03-14 13:33:15 -0300

    feat: finishing class TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class TrackOrders:
 
     def __init__(self):
@@ -10,17 +13,47 @@ class TrackOrders:
         new_order = (customer, order, day)
         self._orders.append(new_order)
 
+    def track_data_from_orders(self, i, customer=None):
+        data_orders = [
+            order[i]
+            for order in self._orders
+            if not customer or order[0] == customer
+        ]
+        return data_orders
+
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        ordered_dishes = self.track_data_from_orders(1, customer)
+
+        if not ordered_dishes:
+            return None
+
+        dish_counts = {}
+        for dish in ordered_dishes:
+            dish_counts[dish] = dish_counts.get(dish, 0) + 1
+
+        most_ordered_dish = max(dish_counts, key=dish_counts.get)
+        return most_ordered_dish
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_dishes = set(self.track_data_from_orders(1))
+        customer_dishes = set(self.track_data_from_orders(1, customer))
+        never_ordered = all_dishes - customer_dishes
+        return never_ordered
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        all_days = set(self.track_data_from_orders(2))
+        customer_days = set(self.track_data_from_orders(2, customer))
+        never_visited = all_days - customer_days
+        return never_visited
 
     def get_busiest_day(self):
-        pass
+        all_days = self.track_data_from_orders(2)
+        max_days = Counter(all_days).most_common()
+        max_day = max_days[0][0]
+        return max_day
 
     def get_least_busy_day(self):
-        pass
+        all_days = self.track_data_from_orders(2)
+        max_days = Counter(all_days).most_common()
+        min_day = max_days[-1][0]
+        return min_day
